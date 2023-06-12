6567036a2305ed6e7bc67f573180a2c67ee27b0d
NandurkarNeil <87815475+Vishal220796@users.noreply.github.com>
2023-06-12 18:44:07 +0530

    Add files via upload

---
+++ Web.py
@@ -0,0 +1,15 @@
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+
+API_url = 'https://mainnet.infura.io/v3/ec5acb1175dc468c9f3ee9a84a02fe98'
+web3 = Web3(Web3.HTTPProvider(API_url))
+
+Block_data = web3.eth.getBlock(12964964)
+print('Block data:',Block_data)
+
+print('gasUsed:',Block_data ['gasUsed'])
+print('Total difficulty:',Block_data ['difficulty'])
+
+print('transaction data',Block_data['transactions'])
+transaction = web3.eth.get_transaction('0x4121669c46c143d17eb2233c8cd73ef52125cb5f0e352bac13093a628f958890')
+print('data',transaction)
